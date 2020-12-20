da69c32f6e73af6d2a304a979a7a265e33ebc331
potados99 <potados99@gmail.com>
2020-12-21 06:13:06 +0900

    Add test.

--- playground/ContentView.swift
+++ playground/ContentView.swift
@@ -8,7 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
+
     var body: some View {
+        Text("Haha yeah!")
+            .padding()
+
         Text("Hello, world!")
             .padding()
     }
