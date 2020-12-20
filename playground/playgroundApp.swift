11c9caa1ec61cdfa05ef21dd633d99022617a353
potados99 <potados99@gmail.com>
2020-12-21 05:50:25 +0900

    Initial Commit

---
+++ playground/playgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  playgroundApp.swift
+//  playground
+//
+//  Created by POTADOS on 2020/12/21.
+//
+
+import SwiftUI
+
+@main
+struct playgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
